47c4c998439a7b753e9f0de6da518fe98c2a6929
Aruna kadugu <arunakumari.7k@gmail.com>
2017-10-11 13:46:28 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //enter full name, age
+        Scanner sc = new Scanner( System.in);
+        string name;
+        int age;
+        System.out.println("enter your name:");
+        name = input.nextString();
+        System.out.println("age");
+        age = input.nextInt();
+        System.out.println("your name:" +name+ "your age:" +age+);
+
+
+    }
+}
